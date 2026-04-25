61cbf7c954fab8ac941fb5a24b74e2ec32c49766
Inf1nlty <60602644+Inf1nlty@users.noreply.github.com>
2026-04-26 00:46:14 +0800

    v2.0.25

--- src/main/java/net/xiaoyu233/mitemod/miteite/trans/render/RenderLightningBoltTrans.java
+++ src/main/java/net/xiaoyu233/mitemod/miteite/trans/render/RenderLightningBoltTrans.java
@@ -13,7 +13,7 @@ public class RenderLightningBoltTrans {
     @Redirect(method = "doRenderLightningBolt", at = @At(value = "FIELD", target = "Lnet/minecraft/Minecraft;raining_strength_for_render_view_entity:F"))
     private float allowRenderLightningInNoSky(Minecraft minecraft, EntityLightningBolt lightning, double x, double y, double z, float yaw, float partialTicks) {
         float rainStrength = minecraft.raining_strength_for_render_view_entity;
-        if (lightning != null && lightning.worldObj != null && lightning.worldObj.provider != null && lightning.worldObj.provider.hasNoSky) {
+        if (lightning != null && lightning.worldObj != null) {
             return Math.max(rainStrength, 1.0F);
         }
         return rainStrength;
